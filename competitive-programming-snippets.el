3911e3246b3690a3c65ad0400348a1fb38d1f1c9
Seong Yong-ju <sei40kr@gmail.com>
2018-07-17 21:45:30 +0900

    Format competitive-programming-snippets.el

--- competitive-programming-snippets.el
+++ competitive-programming-snippets.el
@@ -4,13 +4,13 @@
 
 (defconst competitive-programming-snippets-dir
   (expand-file-name
-    "snippets"
-    (file-name-directory
-      (cond
-        (load-in-progress load-file-name)
-        ((and (boundp 'byte-compile-current-file) byte-compile-current-file)
-          byte-compile-current-file)
-        (:else (buffer-file-name))))))
+   "snippets"
+   (file-name-directory
+    (cond
+     (load-in-progress load-file-name)
+     ((and (boundp 'byte-compile-current-file) byte-compile-current-file)
+      byte-compile-current-file)
+     (:else (buffer-file-name))))))
 
 (defun competitive-programming-snippets-initialize ()
   (add-to-list 'yas-snippet-dirs 'competitive-programming-snippets-dir t)
